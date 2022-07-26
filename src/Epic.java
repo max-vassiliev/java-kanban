cb30fe0e1ad8a52e42934bd0e866eca1ef1a66ac
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-07-26 18:38:50 +0300

    refactor: добавить классу Epic наследование от Task и суперконструктор с параметрами от класса-родителя

--- src/Epic.java
+++ src/Epic.java
@@ -1,2 +1,9 @@
-public class Epic {
+import java.util.ArrayList;
+
+public class Epic extends Task {
+//    ArrayList<Integer> subtasks = new ArrayList<>();
+
+    public Epic(int id, String title, String description, String status) {
+        super(id, title, description, status);
+    }
 }
