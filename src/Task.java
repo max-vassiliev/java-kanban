9a31b429585edf2ee48a95dd563e513d17234b35
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-07-26 17:55:37 +0300

    feat: в класс Task добавить поля и создать конструктор с параметрами id, title, description, status

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,13 @@
 public class Task {
+    int id;
+    String title;
+    String description;
+    String status;
+
+    public Task(int id, String title, String description, String status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
 }
