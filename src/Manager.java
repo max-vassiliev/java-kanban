57e641990c56d8ff9da41672b4febd7fcfa9c892
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-07-29 12:10:13 +0300

    feat: создать хеш-таблицы для хранения задач разных типов

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,16 @@
+import entries.Epic;
+import entries.Subtask;
+import entries.Task;
+
+import java.util.HashMap;
+
 public class Manager {
     private Integer nextId = 1;
 
+    private HashMap<Integer, Task> tasks = new HashMap<>();
+    private HashMap<Integer, Epic> epics = new HashMap<>();
+    private HashMap<Integer, Subtask> subtasks = new HashMap<>();
+
+    
 
 }
