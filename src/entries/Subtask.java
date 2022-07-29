564f5b645887a4a4c30b608579dd0b0cd94d0a88
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-07-29 11:56:37 +0300

    feat: добавить классу подзадачи поля названия и ID родственного эпика и создать конструктор

--- src/entries/Subtask.java
+++ src/entries/Subtask.java
@@ -1,7 +1,13 @@
 package entries;
 
 public class Subtask extends Task {
+    protected String relatedEpicTitle;
+    protected int relatedEpicId;
 
+    public Subtask(int id, String title, String description, String status, String relatedEpicTitle) {
+        super(id, title, description, status);
+        this.relatedEpicTitle = relatedEpicTitle;
+    }
 
-
+    
 }
