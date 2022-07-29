28b546d8b0a116bb5a5d53680cd7753a39a551c8
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-07-29 12:12:42 +0300

    feat: создать методы для добавления задач всех типов

--- src/Manager.java
+++ src/Manager.java
@@ -11,6 +11,29 @@ public class Manager {
     private HashMap<Integer, Epic> epics = new HashMap<>();
     private HashMap<Integer, Subtask> subtasks = new HashMap<>();
 
-    
+
+    // ДОБАВИТЬ
+
+    // добавить задачу
+    public void addTask(Task task) {
+        task.setId(nextId++);
+        tasks.put(task.getId(), task);
+    }
+
+    // добавить эпик
+    public void addEpic(Epic epic) {
+        epic.setId(nextId++);
+        checkEpicStatus(epic);
+        epics.put(epic.getId(), epic);
+    }
+
+    // добавить подзадачу
+    public void addSubtask(Subtask subtask) {
+        subtask.setId(nextId++);
+        setEpicSubtaskRelation(subtask);
+        subtasks.put(subtask.getId(), subtask);
+        Epic relatedEpic = epics.get(subtask.getRelatedEpicId());
+        checkEpicStatus(relatedEpic);
+    }
 
 }
