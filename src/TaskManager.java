763ecf52566fe42921f977ff588c56276fd935e1
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-08-09 16:28:58 +0300

    refactor: добавить интерфейс TaskManager

---
+++ src/TaskManager.java
@@ -0,0 +1,59 @@
+import entries.Epic;
+import entries.Subtask;
+import entries.Task;
+
+import java.util.ArrayList;
+
+public interface TaskManager {
+
+    // добавить задачу
+
+    int addTask(Task task);
+
+    int addEpic(Epic epic);
+
+    int addSubtask(Subtask subtask);
+
+    // обновить задачу
+
+    void updateTask(Task task);
+
+    void updateEpic(Epic epic);
+
+    void updateSubtask (Subtask subtask);
+
+    // получить задачу
+
+    Task getTask(int taskId);
+
+    Epic getEpic(int epicId);
+
+    Subtask getSubtask (int subtaskId);
+
+    // получить список задач
+
+    ArrayList<Task> getTasks();
+
+    ArrayList<Epic> getEpics();
+
+    ArrayList<Subtask> getSubtasks();
+
+    ArrayList<Subtask> getSubtasksInEpic(int epicId);
+
+    // удалить задачу
+
+    void deleteTask(Task task);
+
+    void deleteEpic(Epic epic);
+
+    void deleteSubtask(Subtask subtask);
+
+    // удалить все задачи одного типа
+
+    void deleteAllTasks();
+
+    void deleteAllEpics();
+
+    void deleteAllSubtasks();
+
+}
