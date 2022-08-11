f6c3309d3c1a7745b07c9674bb6ef05e4747d142
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-08-11 14:11:13 +0300

    refactor: сделать все мапы в полях final

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -11,9 +11,9 @@ import java.util.Map;
 
 public class InMemoryTaskManager implements TaskManager {
     private Integer nextId = 1;
-    private Map<Integer, Task> tasks = new HashMap<>();
-    private Map<Integer, Epic> epics = new HashMap<>();
-    private Map<Integer, Subtask> subtasks = new HashMap<>();
+    private final Map<Integer, Task> tasks = new HashMap<>();
+    private final Map<Integer, Epic> epics = new HashMap<>();
+    private final Map<Integer, Subtask> subtasks = new HashMap<>();
 
 
     // ДОБАВИТЬ
