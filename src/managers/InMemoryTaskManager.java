ec3e3d87cb7678af1d6d468c939dcd33130c71eb
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-08-11 14:13:39 +0300

    feat: добавить в поля объект менеджера истории

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -14,6 +14,7 @@ public class InMemoryTaskManager implements TaskManager {
     private final Map<Integer, Task> tasks = new HashMap<>();
     private final Map<Integer, Epic> epics = new HashMap<>();
     private final Map<Integer, Subtask> subtasks = new HashMap<>();
+    private final HistoryManager historyManager = Managers.getDefaultHistory(); 
 
 
     // ДОБАВИТЬ
