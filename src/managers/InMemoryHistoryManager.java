a2ae116d802caa79da3519cf9410d0f3dabd1dbb
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-08-11 14:00:23 +0300

    feat: добавить поля

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -1,4 +1,17 @@
 package managers;
 
+import tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemoryHistoryManager implements HistoryManager {
+
+    private static final int MAX_HISTORY = 10;
+
+    private final List<Task> history = new ArrayList<>();
+
+       
+
+
 }
