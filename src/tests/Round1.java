b0957eebd88997f29b4f979627b47d1731a8681b
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-08-12 21:45:18 +0300

    delete: убрать вызов id в тестах

--- src/tests/Round1.java
+++ src/tests/Round1.java
@@ -10,32 +10,28 @@ public class Round1 {
 
     // создать задачу task1
     public static Task createTask1() {
-        return new Task(0,
-                "Позвонить бабушке",
+        return new Task("Позвонить бабушке",
                 "Дозвониться до бабушки. Узнать, как дела.",
                 "NEW");
     }
 
     // создать задачу task2
     public static Task createTask2() {
-        return new Task(0,
-                "Написать маме",
+        return new Task("Написать маме",
                 "Написать маме сообщение о том, как дела у бабушки.",
                 "NEW");
     }
 
     // создать эпик epic1
     public static Epic createEpic1() {
-        return new Epic(0,
-                "Навестить бабушку",
+        return new Epic("Навестить бабушку",
                 "Съездить к бабушке, привезти продукты, помочь по дому.",
                 null);
     }
 
     // создать эпик epic2
     public static Epic createEpic2() {
-        return new Epic(0,
-                "Спринт 3",
+        return new Epic("Спринт 3",
                 "Пройти теорию, послушать вебинары, сдать проект.",
                 null
         );
@@ -43,8 +39,7 @@ public class Round1 {
 
     // создать для эпика 1 подзадачу 1
     public static Subtask createEpic1Subtask1() {
-        return new Subtask(0,
-                "Купить продукты",
+        return new Subtask("Купить продукты",
                 "Узнать у бабушки, что купить. Сходить в магазин.",
                 "NEW",
                 "Навестить бабушку"
@@ -53,8 +48,7 @@ public class Round1 {
 
     // создать для эпика 1 подзадачу 2
     public static Subtask createEpic1Subtask2() {
-        return new Subtask(0,
-                "Съездить к бабушке",
+        return new Subtask("Съездить к бабушке",
                 "Привезти бабушке продукты, вместе попить чай, помочь по дому.",
                 "NEW",
                 "Навестить бабушку"
@@ -63,8 +57,7 @@ public class Round1 {
 
     // создать для эпика 2 подзадачу 1
     public static Subtask createEpic2Subtask1() {
-        return new Subtask(0,
-                "Сдать ДЗ",
+        return new Subtask("Сдать ДЗ",
                 "Доделать финальный проект и отправить на проверку.",
                 "NEW",
                 "Спринт 3"
--- src/tests/Round2.java
+++ src/tests/Round2.java
@@ -8,20 +8,16 @@ public class Round2 {
     // МЕНЯЕМ СТАТУСЫ НЕКОТОРЫХ ЗАДАЧ
 
     // в задаче task1 меняем статус на DONE
-    public static Task updateTask1(Task task) {
-        int taskId = task.getId();
-        return new Task(taskId,
-                "Позвонить бабушке",
+    public static Task updateTask1() {
+        return new Task("Позвонить бабушке",
                 "Дозвониться до бабушки. Узнать, как дела.",
                 "DONE"); // новый статус
     }
 
     // в подзадаче 1 эпика 1 меняем статус на IN_PROGRESS
     public static Subtask updateEpic1Subtask1(Subtask subtask) {
-        int subtaskId = subtask.getId();
         int relatedEpicId = subtask.getRelatedEpicId();
-        Subtask subtaskUpdate = new Subtask(subtaskId,
-                "Купить продукты",
+        Subtask subtaskUpdate = new Subtask("Купить продукты",
                 "Узнать у бабушки, что купить. Сходить в магазин.",
                 "IN_PROGRESS", // новый статус
                 "Навестить бабушку");
@@ -33,8 +29,7 @@ public class Round2 {
     public static Subtask updateEpic2Subtask1(Subtask subtask) {
         int subtaskId = subtask.getId();
         int relatedEpicId = subtask.getRelatedEpicId();
-        Subtask subtaskUpdate = new Subtask(subtaskId,
-                "Сдать ДЗ",
+        Subtask subtaskUpdate = new Subtask("Сдать ДЗ",
                 "Доделать финальный проект и отправить на проверку.",
                 "DONE", // новый статус
                 "Спринт 3");
