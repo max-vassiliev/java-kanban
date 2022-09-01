9dca54523fe6722ec0dc1b0632dc66c22e5ba4f7
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-09-01 21:42:58 +0300

    feat/добавить класс для связного списка

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -3,10 +3,7 @@ package managers;
 import tasks.Task;
 import utilities.Node;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
@@ -31,4 +28,62 @@ public class InMemoryHistoryManager implements HistoryManager {
         return history;
     }
 
+    class CustomLinkedList<T> {
+        private Node<Task> head;
+        private Node<Task> tail;
+
+        // добавить задачу в историю просмотров
+        public void linkLast(Task task) {
+            int id = task.getId();
+
+            if (historyMap.containsKey(id)) {
+                removeNode(historyMap.get(id));
+            }
+
+            final Node<Task> oldTail = tail;
+            final Node<Task> newNode = new Node<>(oldTail, task, null);
+            tail = newNode;
+
+            if (oldTail == null) {
+                head = newNode;
+            } else {
+                oldTail.next = newNode;
+            }
+            historyMap.put(id, newNode);
+        }
+
+        // получить все задачи из истории просмотров
+        public List<Task> getTasks() {
+            Node<Task> curHead = head;
+            List<Task> elementsInList = new ArrayList<>();
+
+            while (curHead != null) {
+                elementsInList.add(curHead.data);
+                curHead = curHead.next;
+            }
+            return elementsInList;
+        }
+
+        // удалить узел из истории
+        private void removeNode(Node<Task> node) {
+            if (node == null) {
+                return;
+            }
+            if (node.prev == null) {
+                Node<Task> nextNode = node.next;
+                nextNode.prev = null;
+                head = nextNode;
+            } else if (node.next == null) {
+                Node<Task> prevNode = node.prev;
+                prevNode.next = null;
+                tail = prevNode;
+            } else {
+                Node<Task> nextNode = node.next;
+                Node<Task> prevNode = node.prev;
+                prevNode.next = nextNode;
+                nextNode.prev = prevNode;
+            }
+            node = null;
+        }
+    }
 }
