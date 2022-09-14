748635c48967911b63d7d35890ce94941d1f3344
Maxim Vassiliev <m.vassiliev@yandex.ru>
2022-09-14 15:02:02 +0300

    feat: добавить поля и метод main

--- src/managers/FileBackedTaskManager.java
+++ src/managers/FileBackedTaskManager.java
@@ -1,4 +1,16 @@
 package managers;
 
+import java.nio.file.Path;
+
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
+
+    private static final String HOME = "resources/";
+
+    private Path history;
+
+    
+    static void main(String[] args) {
+
+    }
+
 }
